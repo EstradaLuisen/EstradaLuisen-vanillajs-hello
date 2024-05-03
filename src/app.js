dff44ad98e756a00770f37299a94d69b027ea049
EstradaLuisen <143045575+EstradaLuisen@users.noreply.github.com>
2024-05-03 05:10:36 +0000

    ADD pronouns adj nouns and domain

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let domains = [".com", ".net", ".us", ".io"];
+
+  for (let pronoun in pronouns) {
+    for (let adj in adjs) {
+      for (let noun in nouns) {
+        for (let domain in domains) {
+          console.log(
+            pronouns[pronoun] + adjs[adj] + nouns[noun] + domains[domain]
+          );
+        }
+      }
+    }
+  }
+
   console.log("Hello Rigo from the console!");
 };
